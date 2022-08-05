35215c9b5d813e3bbff6c37dd7647b2c9af8c502
imam <ahmadimamfauzi@gmail.com>
2022-08-05 14:12:33 +0700

    auto push

--- js/script.js
+++ js/script.js
@@ -24,35 +24,104 @@ $(document).ready(function () {
     $("#tabs .right").removeClass("show");
     $(sel).addClass("show");
     $(this).addClass("active");
+    var index = data.indexOf(sel);
+    selecteditem = index;
+    if (selecteditem < total - 1) {
+      $(".nextTab").addClass("selactive");
+    } else if (selecteditem == total - 1) {
+      $(".nextTab").removeClass("selactive");
+    }
+    if (selecteditem > 0) {
+      $(".prevTab").addClass("selactive");
+    } else if (selecteditem == 0) {
+      $(".prevTab").removeClass("selactive");
+    }
+    $("select.change").val(sel).change();
   });
-  
-var option = "";
-var active = "";
-var selecteditem = 0;
-var total = 0;
-var tabctrl = $('.tabctrl');
-tabctrl.each(function(index){
-  var target = $(this).attr("data-target");
-var label = $(this).html();
-var cname = $(this).attr("class");
-var scname = JSON.stringify(cname);
-if(scname.includes("active")){
-  active = label;
-  selecteditem = index;
-}
-total++;
-option += "<option value='"+target+"'>"+label+"</option>";
-});
-console.log(selecteditem);
-if(selecteditem < total){
-  $(".nextTab").addClass("selactive");
-}else if(selecteditem == total){
-  $(".nextTab").removeClass("selactive");
-}
-if(selecteditem > 0){
-  $(".prevTab").addClass("selactive");
-}else if(selecteditem == 0){
-  $(".prevTab").removeClass("selactive");
-}
-$("select.change").html(option);
+  var data = [];
+  var option = "";
+  var active = "";
+  var selecteditem = 0;
+  var total = 0;
+  var tabctrl = $(".tabctrl");
+  tabctrl.each(function (index) {
+    var target = $(this).attr("data-target");
+    var label = $(this).html();
+    var cname = $(this).attr("class");
+    data.push(target);
+    if (cname == "tabctrl active") {
+      active = label;
+      selecteditem = index;
+      option +=
+        "<option value='" + target + "' selected>" + label + "</option>";
+    } else {
+      option += "<option value='" + target + "'>" + label + "</option>";
+    }
+    total++;
+  });
+  if (selecteditem < total - 1) {
+    $(".nextTab").addClass("selactive");
+  } else if (selecteditem == total - 1) {
+    $(".nextTab").removeClass("selactive");
+  }
+  if (selecteditem > 0) {
+    $(".prevTab").addClass("selactive");
+  } else if (selecteditem == 0) {
+    $(".prevTab").removeClass("selactive");
+  }
+  $("select.change").html(option);
+  $(".nextTab").on("click", function () {
+    if (selecteditem < total - 1) {
+      selecteditem++;
+      var sel = data[selecteditem];
+      $("select.change").val(sel).change();
+      changetab(sel, selecteditem);
+      if (selecteditem == total - 1) {
+        $(".nextTab").removeClass("selactive");
+      }
+      if (selecteditem > 0) {
+        $(".prevTab").addClass("selactive");
+      }
+    } else {
+      $(".nextTab").removeClass("selactive");
+    }
+  });
+  $(".prevTab").on("click", function () {
+    if (selecteditem > 0) {
+      selecteditem--;
+      var sel = data[selecteditem];
+      $("select.change").val(sel).change();
+      changetab(sel, selecteditem);
+      if (selecteditem == 0) {
+        $(".prevTab").removeClass("selactive");
+      }
+      if (selecteditem < total) {
+        $(".nextTab").addClass("selactive");
+      }
+    } else {
+      $(".prevTab").removeClass("selactive");
+    }
+  });
+  $("select.change").on("change", function () {
+    var sel = $(this).val();
+    var index = data.indexOf(sel);
+    selecteditem = index;
+    if (selecteditem < total - 1) {
+      $(".nextTab").addClass("selactive");
+    } else if (selecteditem >= total - 1) {
+      $(".nextTab").removeClass("selactive");
+    }
+    if (selecteditem > 0) {
+      $(".prevTab").addClass("selactive");
+    } else if (selecteditem == 0) {
+      $(".prevTab").removeClass("selactive");
+    }
+    changetab(sel, index);
+  });
+  function changetab(sel, index) {
+    $("#tabs li a").removeClass("active");
+    $("#tabs .right").removeClass("show");
+    $(sel).addClass("show");
+    document.getElementsByClassName("tabctrl")[index].classList.add("active");
+  }
 });
