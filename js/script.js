4b79ef78759b1a3f016402f8b21686f24badb7bf
imam <ahmadimamfauzi@gmail.com>
2022-08-05 12:46:07 +0700

    auto push

--- js/script.js
+++ js/script.js
@@ -1,28 +1,58 @@
 document.addEventListener("DOMContentLoaded", function () {
-    window.addEventListener("scroll", function () {
-      var scroll = $(window).scrollTop();
-      if (scroll > 50) {
-        document.getElementById("topnav").classList.add("fixed-top");
-        // add padding top to show content behind navbar
-      } else {
-        document.getElementById("topnav").classList.remove("fixed-top");
-        // remove padding top from body
-      }
-    });
+  window.addEventListener("scroll", function () {
+    var scroll = $(window).scrollTop();
+    if (scroll > 50) {
+      document.getElementById("topnav").classList.add("fixed-top");
+      // add padding top to show content behind navbar
+    } else {
+      document.getElementById("topnav").classList.remove("fixed-top");
+      // remove padding top from body
+    }
   });
-  // DOMContentLoaded  end
+});
+// DOMContentLoaded  end
 
-$(document).ready(function (){
+$(document).ready(function () {
   $(".btnmenu").click(function () {
     $(this).parent().parent().toggleClass("show");
     $(".mobile-container").slideToggle("fast");
     $("body").toggleClass("overlay");
   });
-  $("#tabs a").click(function(){
-    var sel = $(this).data('target');
-    $('#tabs li a').removeClass("active");
-    $('#tabs .right').removeClass("show");
+  $("#tabs li a").click(function () {
+    var sel = $(this).data("target");
+    $("#tabs li a").removeClass("active");
+    $("#tabs .right").removeClass("show");
     $(sel).addClass("show");
     $(this).addClass("active");
-  })
+  });
+  
+var option = "";
+var active = "";
+var selecteditem = 0;
+var total = 0;
+var tabctrl = $('.tabctrl');
+tabctrl.each(function(index){
+  var target = $(this).attr("data-target");
+var label = $(this).html();
+var cname = $(this).attr("class");
+var scname = JSON.stringify(cname);
+if(scname.includes("active")){
+  active = label;
+  selecteditem = index;
+}
+total++;
+option += "<option value='"+target+"'>"+label+"</option>";
+});
+console.log(selecteditem);
+if(selecteditem < total){
+  $(".nextTab").addClass("selactive");
+}else if(selecteditem == total){
+  $(".nextTab").removeClass("selactive");
+}
+if(selecteditem > 0){
+  $(".prevTab").addClass("selactive");
+}else if(selecteditem == 0){
+  $(".prevTab").removeClass("selactive");
+}
+$("select.change").html(option);
 });
---
+++ js/slide.js
@@ -0,0 +1,38 @@
+let slideIndex = 0;
+  autoSlides();
+
+  function plusSlides(n) {
+    showSlides((slideIndex += n));
+  }
+
+  function currentSlide(n) {
+    showSlides((slideIndex = n));
+  }
+  function autoSlides() {
+    let i;
+    let slides = document.getElementsByClassName("mySlide");
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {
+      slideIndex = 1;
+    }
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(autoSlides, 2000); // Change image every 2 seconds
+  }
+
+  function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlide");
+    if (n > slides.length) {
+      slideIndex = 1;
+    }
+    if (n < 1) {
+      slideIndex = slides.length;
+    }
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    slides[slideIndex - 1].style.display = "block";
+  }
